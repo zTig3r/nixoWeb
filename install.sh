ad6ea6f0b3527e2b24fabeaef7c9afcd23628bcc
zTig3r <Timo1Bauer@gmx.de>
2024-01-21 14:55:08 +0100

    Update docker-compose command in install.sh

--- install.sh
+++ install.sh
@@ -2,6 +2,6 @@
 
 sudo apt-get update && sudo apt-get upgrade -y && curl -fsSL https://get.docker.com -o get-docker.sh | sh && sudo apt-get install -y docker-compose
 
-docker-compose up -d
+docker-compose up --build -d
 
 echo -e "\e[32mScript completed successfully.\e[0m"
