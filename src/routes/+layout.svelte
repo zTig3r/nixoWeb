448cb5a573ffc6b3a0d89cf08b6a884bc00bd924
Timo Bauer <108749108+zTig3r@users.noreply.github.com>
2025-09-13 14:07:06 +0200

    Update copyright year from 2024 to 2025

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -94,7 +94,7 @@
       </div>
       <hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
       <div class="sm:flex sm:items-center sm:justify-between">
-        <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2024 NixoCraft</span>
+        <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2025 NixoCraft</span>
         <ul class="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0">
           <li>
             <a href="/privacy" class="hover:underline me-4 md:me-6">Datenschützerklärung</a>
