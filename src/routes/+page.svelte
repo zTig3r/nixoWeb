d901a8dd62628f103be5e11c3a6dd4e3535b6da9
zTig3r <timo1bauer@gmx.de>
2025-11-09 22:53:03 +0100

    Add NixoCraft 4, add NixoCraft 5 Countdown

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,18 +2,19 @@
     import {onMount} from "svelte";
     import {writable} from "svelte/store";
     import {title} from "$lib/titleStore";
+    import Countdown from "$lib/countdown.svelte";
 
-    import logo from "$lib/assets/logo.webp";
+    import nixoCraftLogo from "$lib/assets/nixocraft5.webp";
 
-    // TODO: NixoCraft 4 Bilder hinzufügen
     import image1 from "$lib/assets/slideShow/image1.jpg";
     import image2 from "$lib/assets/slideShow/image2.jpg";
     import image3 from "$lib/assets/slideShow/image3.jpg";
     import image4 from "$lib/assets/slideShow/image4.jpg";
     import image5 from "$lib/assets/slideShow/image5.jpg";
     import image6 from "$lib/assets/slideShow/image6.jpg";
+    import image7 from "$lib/assets/slideShow/image7.jpg";
 
-    let images = [image1, image2, image3, image4, image5, image6];
+    let images = [image1, image2, image3, image4, image5, image6, image7];
     let currentImageIndex = writable(0);
 
     onMount(() => {
@@ -44,17 +45,11 @@
 
     <div class="relative bg-black/60 w-full min-h-screen">
         <div class="flex flex-col max-w-2xl justify-center mx-auto h-screen shadow" style="height: calc(100vh - 50px);">
-            <img src={logo} alt="Logo" class="w-auto scale-90"/>
-
-            <!-- TODO: Bei NixoCraft 4 Ende aktivieren
-
             <img src={nixoCraftLogo} alt="Logo" class="w-auto scale-90"/>
 
             <div class="text-center mt-10 text-shadow">
                 <Countdown></Countdown>
             </div>
-
-            -->
         </div>
     </div>
 </section>
